fe221455ee4c0df9340568b7eab90e5a3b6771fa
Eranjan Kandegedara <36378725+ebkera@users.noreply.github.com>
2020-10-07 08:40:10 -0500

    Update master_installation.sh

--- master_installation.sh
+++ master_installation.sh
@@ -64,14 +64,14 @@ make DYNAMIC_ARCH=0 CC=gcc FC=gfortran HOSTCC=gcc BINARY=64 INTERFACE=64 \
 make PREFIX=$OPENBLAS_DIR LIBNAMESUFFIX=nonthreaded install
 
 #Below is the installation for blas version 0.3.10 and for the parallel installation
-wget -O OpenBLAS.tar.gz https://github.com/xianyi/OpenBLAS/archive/v0.3.10.tar.gz
-tar xzf OpenBLAS.tar.gz && rm OpenBLAS.tar.gz
-cd OpenBLAS-0.3.10
-make DYNAMIC_ARCH=0 CC=gcc FC=gfortran HOSTCC=gcc BINARY=64 INTERFACE=64 \
-  NO_AFFINITY=1 NO_WARMUP=1 USE_OPENMP=0 USE_THREAD=0 USE_LOCKING=1 LIBNAMESUFFIX=nonthreaded
-make PREFIX=$OPENBLAS_DIR LIBNAMESUFFIX=nonthreaded install
+# wget -O OpenBLAS.tar.gz https://github.com/xianyi/OpenBLAS/archive/v0.3.10.tar.gz
+# tar xzf OpenBLAS.tar.gz && rm OpenBLAS.tar.gz
+# cd OpenBLAS-0.3.10
+# make DYNAMIC_ARCH=0 CC=gcc FC=gfortran HOSTCC=gcc BINARY=64 INTERFACE=64 \
+#   NO_AFFINITY=1 NO_WARMUP=1 USE_OPENMP=0 USE_THREAD=0 USE_LOCKING=1 LIBNAMESUFFIX=nonthreaded
+# make PREFIX=$OPENBLAS_DIR LIBNAMESUFFIX=nonthreaded install
 
-# if proerly done you should get somehign like this  ### start
+# if proerly done you should get somehing like this  ### start
 #make[1]: Leaving directory '/opt/openblas/OpenBLAS-0.3.10/exports'
 
 # OpenBLAS build complete. (BLAS CBLAS LAPACK LAPACKE)
